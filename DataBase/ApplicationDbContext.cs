c2c1bc38e8ca2c490d6822c89c324d42b823dd3e
aris <aris.prnm16@gmail.com>
2024-08-11 16:39:42 +0700

    first commit

---
+++ DataBase/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ApiCrud.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiCrud.DataBase
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Products> Products { get; set; }
+    }
+}
---
+++ Models/Products.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ApiCrud.Models
+{
+    public class Products
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public  string Description { get; set; }
+    }
+    public class ProductsRequest
+    {
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public string Description { get; set; }
+    }
+    public class UpdateProductsRequest
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public string Description { get; set; }
+    }
+}
